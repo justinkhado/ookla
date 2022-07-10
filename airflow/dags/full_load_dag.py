1da79a73aaac481305877d6c57059d3171ed43b9
justinkhado <justinkhado@gmail.com>
2022-07-09 20:10:02 -0700

    configurations

--- airflow/dags/full_load_dag.py
+++ airflow/dags/full_load_dag.py
@@ -1,9 +1,12 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 
 import logging
 import geopandas
+
+OOKLA_S3_URL = 'https://ookla-open-data.s3.amazonaws.com/parquet/performance/type={fixed,mobile}/year=2020/quarter=[1-4]/2020-{01,04,07,10}-01_performance_{fixed,mobile}_tiles.parquet'
 
 def print_geopandas_version():
     logging.info(geopandas.__version__)
@@ -23,9 +26,9 @@ with DAG(
         python_callable=print_geopandas_version
     )
 
-    test_task_2 = PythonOperator(
-        task_id='test_task_2',
-        python_callable=print_geopandas_version
+    download_ookla_data_op = BashOperator(
+        task_id='download_dataset',
+        bash_command=f'curl -sSLf {OOKLA_S3_URL}'
     )
 
-    test_task >> test_task_2
+    test_task >> download_ookla_data_op
